b99213487803d565d78033878bd74f1fa39c33b1
Caleb Brock <cbrock@csus.edu>
2025-10-17 14:19:25 -0700

    EI-2 - Prepend /api to controller components

--- endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/controller/BatchesController.java
+++ endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/controller/BatchesController.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/batches")
+@RestController("/api/batches")
 public class BatchesController {
 
 	private final static Logger LOG = LoggerFactory.getLogger(BatchesController.class);
--- endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/controller/JobsController.java
+++ endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/controller/JobsController.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/jobs")
+@RestController("/api/jobs")
 public class JobsController {
 
 	private final static Logger LOG = LoggerFactory.getLogger(JobsController.class);
