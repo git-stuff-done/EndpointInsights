6dbc57d38007c27f492e053e202984f09c6153bb
Caleb Brock <cbrock@csus.edu>
2025-10-17 09:04:06 -0700

    EI-2 - Create jobs and batches controllers

---
+++ endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/controller/JobsController.java
@@ -0,0 +1,12 @@
+package com.vsp.endpointinsightsapi.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/jobs")
+public class JobsController {
+
+	private final static Logger LOG = LoggerFactory.getLogger(JobsController.class);
+
+}
