87439a1ce0274464dce9177db994ade8231cb7a5
Caleb Brock <cbrock@csus.edu>
2025-10-17 18:06:34 -0700

    EI-42 - Add unit tests for controller

--- endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/controller/JobsController.java
+++ endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/controller/JobsController.java
@@ -9,12 +9,14 @@ import jakarta.validation.constraints.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/jobs")
+@Validated
 public class JobsController {
 
 	private final static Logger LOG = LoggerFactory.getLogger(JobsController.class);
--- endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/exception/GlobalExceptionHandler.java
+++ endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/exception/GlobalExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.vsp.endpointinsightsapi.exception;
 
-import jakarta.validation.ValidationException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -19,9 +21,18 @@ public class GlobalExceptionHandler {
 	}
 
 	// Map validation exceptions to a specific, reusable format for easy error parsing
-	@ExceptionHandler(ValidationException.class)
-	public ResponseEntity<ErrorResponse> handleValidationException(ValidationException e) {
-		CustomException ce = (new CustomExceptionBuilder(e.getMessage())).withStatus(HttpStatus.BAD_REQUEST).build();
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<ErrorResponse> handleValidationException(ConstraintViolationException e) {
+		String[] details = e.getConstraintViolations().stream().map(ConstraintViolation::getMessage).toArray(String[]::new);
+		CustomException ce = (new CustomExceptionBuilder(details))
+				.withStatus(HttpStatus.BAD_REQUEST).build();
+		return handleCustomException(ce);
+	}
+
+	@ExceptionHandler(ResponseStatusException.class)
+	public ResponseEntity<ErrorResponse> handleResponseStatusException(ResponseStatusException e) {
+		CustomException ce = (new CustomExceptionBuilder(e.getMessage()))
+				.withStatus(HttpStatus.valueOf(e.getStatusCode().value())).build();
 		return handleCustomException(ce);
 	}
 
