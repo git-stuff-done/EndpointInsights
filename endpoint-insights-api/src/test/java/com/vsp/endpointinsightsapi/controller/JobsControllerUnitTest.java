a2f3206c1ed03bc8b747ea555870d9c827140c38
Brynn Crowley <littlehill723@gmail.com>
2025-10-17 20:14:02 -0700

    feat: configure jenkinsfile for unit testing pipline (EI-15)
    Signed-off-by: Brynn Crowley <littlehill723@gmail.com>

---
+++ endpoint-insights-api/src/test/java/com/vsp/endpointinsightsapi/controller/JobsControllerUnitTest.java
@@ -0,0 +1,19 @@
+package com.vsp.endpointinsightsapi.controller;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@Tag("unit")
+@WebMvcTest(JobsController.class)
+class JobsControllerUnitTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void contextLoad() {
+
+    }
+}
