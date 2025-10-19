a36bb551d98de683516ceee44f548304cbfd32a9
cGod778 <cjrankin@csus.edu>
2025-10-19 11:22:10 -0700

    Update BatchesController.java

--- endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/controller/BatchesController.java
+++ endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/controller/BatchesController.java
@@ -1,5 +1,8 @@
 package com.vsp.endpointinsightsapi.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
 import com.vsp.endpointinsightsapi.dto.BatchRequestDTO;
 import com.vsp.endpointinsightsapi.dto.BatchResponseDTO;
 import org.springframework.web.bind.annotation.*;
