edf9484d33a28fa6dcec4960a9e9403f7b167842
Marcos Pantoja <marcospantoja21@outlook.com>
2025-10-20 03:24:14 -0700

    added lombook support for getters and setters, changed application.properties to application.yml, added @notebook notation.

--- endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/testbatchtable/TestBatchTable.java
+++ endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/testbatchtable/TestBatchTable.java
@@ -2,10 +2,16 @@ package com.vsp.endpointinsightsapi.testbatchtable;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
 
 //import com.vsp.endpointinsightsapi.schedule.JobSchedule;
 //import com.vsp.endpointinsightsapi.testrun.TestRun;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "test_batch")
 public class TestBatchTable {
@@ -23,61 +29,12 @@ public class TestBatchTable {
 //    private List<TestRun> testRuns;
 
     @Column(name = "created_at")
-    private LocalDateTime createdAt = LocalDateTime.now();
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt = new Date();
 
     @Column(name = "status")
     private String status;
 
     @Column(name = "notes")
     private String notes;
-
-    // --- Getters & Setters ---
-
-    public Long getBatchId() {
-        return batchId;
-    }
-
-    public void setBatchId(Long batchId) {
-        this.batchId = batchId;
-    }
-
-//    public JobSchedule getJobSchedule() {
-//        return jobSchedule;
-//    }
-
-//    public void setJobSchedule(JobSchedule jobSchedule) {
-//        this.jobSchedule = jobSchedule;
-//    }
-
-//    public List<TestRun> getTestRuns() {
-//        return testRuns;
-//    }
-
-//    public void setTestRuns(List<TestRun> testRuns) {
-//        this.testRuns = testRuns;
-//    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
 }
