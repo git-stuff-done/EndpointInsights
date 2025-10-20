7e0b646743175282240aa260cd54b7a1c71394be
2omb Finance <2ombfinance@protonmail.com>
2025-10-19 22:13:34 -0700

    Fixed INTEGRATION typo

--- endpoint-insights-sql/job.sql
+++ endpoint-insights-sql/job.sql
@@ -2,7 +2,7 @@ CREATE TABLE job (
     job_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255) NOT NULL,
     description TEXT,
-    test_type VARCHAR(10) NOT NULL CHECK (test_type IN ('PERF', 'INTERATION', 'E2E')),
+    test_type VARCHAR(10) NOT NULL CHECK (test_type IN ('PERF', 'INTEGRATION', 'E2E')),
     target_id VARCHAR(1024) NOT NULL,
     created_by VARCHAR(255) NOT NULL,
     status VARCHAR(50) DEFAULT 'PENDING' CHECK (status IN ('PENDING', 'RUNNING', 'COMPLETED', 'FAILED')),
