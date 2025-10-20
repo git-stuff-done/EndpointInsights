e1aa69232f3d21858268210a205485f0aa24b9c5
Nick Cooper <nicholascooper@csus.edu>
2025-10-19 22:50:29 -0700

    Batch Card Integration, moved Test Card from forced view into dashboard routing, separate buttons for dashboard and batches made, redesigned UI for readability and made sure future dto backend reading bootstrap was started

--- endpoint-insights-ui/src/main.ts
+++ endpoint-insights-ui/src/main.ts
@@ -1,7 +1,5 @@
-/// <reference types="@angular/localize" />
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent);
-
+bootstrapApplication(AppComponent, appConfig);
