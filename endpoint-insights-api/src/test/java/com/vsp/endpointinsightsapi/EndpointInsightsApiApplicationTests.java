965168c5695678db35288c7fc3bf5e46906edd3b
cGod778 <cjrankin@csus.edu>
2025-10-19 23:15:49 -0700

    Revert "Merge branch 'develop' into feature/EI-41"
    This reverts commit 3e8d133957cee03d00fe71470a0430b66a1872c3, reversing
    changes made to d836803393f2109353ae419358b36a4a6e43e55f.

--- endpoint-insights-api/src/test/java/com/vsp/endpointinsightsapi/EndpointInsightsApiApplicationTests.java
+++ endpoint-insights-api/src/test/java/com/vsp/endpointinsightsapi/EndpointInsightsApiApplicationTests.java
@@ -2,13 +2,12 @@ package com.vsp.endpointinsightsapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class EndpointInsightsApiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
 }
