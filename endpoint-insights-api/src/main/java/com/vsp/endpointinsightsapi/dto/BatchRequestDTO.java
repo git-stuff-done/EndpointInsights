6ae37185cdd2aa0c616c4ad29f9f243991c021db
cGod778 <cjrankin@csus.edu>
2025-10-19 21:13:41 -0700

    (EI-41): finalize test and fixes

--- endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/dto/BatchRequestDTO.java
+++ endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/dto/BatchRequestDTO.java
@@ -1,19 +1,16 @@
 package com.vsp.endpointinsightsapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BatchRequestDTO {
     private String name;
     private String description;
-
-    public BatchRequestDTO() {}
-
-    public BatchRequestDTO(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
 }
--- endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/dto/BatchResponseDTO.java
+++ endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/dto/BatchResponseDTO.java
@@ -1,24 +1,17 @@
 package com.vsp.endpointinsightsapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BatchResponseDTO {
     private Long id;
     private String name;
     private String status;
-
-    public BatchResponseDTO() {}
-
-    public BatchResponseDTO(Long id, String name, String status) {
-        this.id = id;
-        this.name = name;
-        this.status = status;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
 }
