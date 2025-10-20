823798973a744bb9d38cfcbcc58fd29f2f61a243
cbrock-csus <cbrock@csus.edu>
2025-10-19 18:56:59 -0700

    Stub JobsController endpoints (#13)
    * EI-42 - Stub JobsController endpoints and associated code
    
    * EI-42 - Add unit tests for controller

---
+++ endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/model/JobCreateRequest.java
@@ -0,0 +1,13 @@
+package com.vsp.endpointinsightsapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class JobCreateRequest {
+
+	//todo: implement
+}
