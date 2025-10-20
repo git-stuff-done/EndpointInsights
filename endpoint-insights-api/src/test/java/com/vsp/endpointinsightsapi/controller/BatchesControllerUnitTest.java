74c702b40954232d08f2963a5da4285a90a604bb
cbrock-csus <cbrock@csus.edu>
2025-10-19 21:36:16 -0700

    chore: add spring test profile disabling auto config for databases (#24)

--- endpoint-insights-api/src/test/java/com/vsp/endpointinsightsapi/controller/BatchesControllerUnitTest.java
+++ endpoint-insights-api/src/test/java/com/vsp/endpointinsightsapi/controller/BatchesControllerUnitTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(BatchesController.class)
+@ActiveProfiles("test")
 class BatchesControllerUnitTest {
     @Autowired
     private MockMvc mockMvc;
