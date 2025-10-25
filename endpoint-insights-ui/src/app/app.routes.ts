5e9b8692042a35c91d3af40e61fd3d0e54ff63ec
Nicholas Cooper <nicholascooper@csus.edu>
2025-10-24 22:36:30 -0700

    EI-113: create batch card component (#22)

--- endpoint-insights-ui/src/app/app.routes.ts
+++ endpoint-insights-ui/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import {PageNotFoundComponent} from "./page-not-found-component/page-not-found-component";
-import {DashboardComponent} from "./dashboard-component/dashboard-component";
+import { DashboardComponent } from './dashboard-component/dashboard-component';
 
 export const routes: Routes = [
-    { path: '', component: DashboardComponent },
-    { path: '**', component: PageNotFoundComponent }
+    { path: '', component: DashboardComponent, pathMatch: 'full' },
+    { path: 'batches', loadComponent: () => import('./batch-component/batch-component').then(m => m.BatchComponent) },
+    { path: '**', loadComponent: () => import('./page-not-found-component/page-not-found-component').then(m => m.PageNotFoundComponent) },
 ];
