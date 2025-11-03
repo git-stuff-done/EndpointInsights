4c311feb0ce61d1bc40cdc9e0ed88a92ff3070bf
Nicholas Cooper <nicholascooper@csus.edu>
2025-11-02 19:00:34 -0800

    EI-243 - Modal creation along with setting them up in the batch cards for demonstration and beginning of setup for the next sprint. (#29)
    * EI-243 - Modal creation along with setting them up in the batch cards for demonstration and beginning of setup for the next sprint.
    
    * Blunder correction
    
    * Review tweaks
    
    * Review tweaks
    
    * Removed title and title test
    
    ---------
    
    Signed-off-by: Nicholas Cooper <nicholascooper@csus.edu>

--- endpoint-insights-ui/src/app/app.config.ts
+++ endpoint-insights-ui/src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { routes } from './app.routes';
-//import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes, withEnabledBlockingInitialNavigation()),
-        //provideAnimations(),
+        provideAnimations(),
     ],
 };
