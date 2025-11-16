aaaca3acc0927901ae027c7507ff45f7a1d00bd9
Morningstar515 <t.mains515@hotmail.com>
2025-11-15 22:52:02 -0800

    Jpa doesnt like Audit annotation on main class

--- endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/config/EntityAuditConfig.java
+++ endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/config/EntityAuditConfig.java
@@ -3,13 +3,16 @@ package com.vsp.endpointinsightsapi.config;
 
 import com.vsp.endpointinsightsapi.model.UserContext;
 import com.vsp.endpointinsightsapi.util.CurrentUser;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.Optional;
 
 @Configuration
+@EnableJpaAuditing
 public class EntityAuditConfig {
 
     @Bean
--- endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/repository/TestBatchRepository.java
+++ endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/repository/TestBatchRepository.java
@@ -2,10 +2,13 @@ package com.vsp.endpointinsightsapi.repository;
 
 import com.vsp.endpointinsightsapi.model.TestBatch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface TestBatchRepository extends JpaRepository<TestBatch, Long> {
 
-    public TestBatch findByTestId(UUID testId);
+    @Query("SELECT t FROM TestBatch t where t.batch_id = :id")
+    TestBatch findById(@Param("id")UUID id);
 }
