d80c39bd9d16ac45e1c4d7bdf3bc32e25529c40f
Morningstar515 <t.mains515@hotmail.com>
2026-02-21 12:01:43 -0800

    add frontend unit tests

--- endpoint-insights-ui/src/app/services/http-interceptor.service.spec.ts
+++ endpoint-insights-ui/src/app/services/http-interceptor.service.spec.ts
@@ -2,18 +2,40 @@ import { TestBed } from '@angular/core/testing';
 
 import { HttpInterceptorService } from './http-interceptor.service';
 import {provideHttpClient} from "@angular/common/http";
+import {HttpTestingController} from "@angular/common/http/testing";
 
 describe('HttpInterceptorService', () => {
   let service: HttpInterceptorService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [provideHttpClient()]
     });
     service = TestBed.inject(HttpInterceptorService);
+    httpMock = TestBed.inject(HttpTestingController);
+
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+    it('should make a POST request with auth token', () => {
+        service.post('/api/test', { name: 'test' }).subscribe(response => {
+            expect(response).toBeTruthy();
+        });
+        const req = httpMock.expectOne('/api/test');
+        expect(req.request.method).toBe('POST');
+        req.flush({});
+    });
+
+    it('should make a PUT request with auth token', () => {
+        service.put('/api/test', { name: 'test' }).subscribe(response => {
+            expect(response).toBeTruthy();
+        });
+        const req = httpMock.expectOne('/api/test');
+        expect(req.request.method).toBe('PUT');
+        req.flush({});
+    });
 });
