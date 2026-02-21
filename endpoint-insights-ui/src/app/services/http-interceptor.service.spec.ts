2ded576964c484e8b8493409c7ea4be7f0a17b00
Morningstar515 <t.mains515@hotmail.com>
2026-02-21 14:50:09 -0800

    build correction

--- endpoint-insights-ui/src/app/services/http-interceptor.service.spec.ts
+++ endpoint-insights-ui/src/app/services/http-interceptor.service.spec.ts
@@ -3,42 +3,20 @@ import { TestBed } from '@angular/core/testing';
 import { HttpInterceptorService } from './http-interceptor.service';
 import {provideHttpClient} from "@angular/common/http";
 import {HttpTestingController, provideHttpClientTesting} from "@angular/common/http/testing";
+import {BatchApi} from "../batch-component/api/batch-api";
 
 describe('HttpInterceptorService', () => {
   let service: HttpInterceptorService;
-  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [provideHttpClient(),
-         provideHttpClientTesting(),
-         HttpInterceptorService,
       ]
     });
     service = TestBed.inject(HttpInterceptorService);
-    httpMock = TestBed.inject(HttpTestingController);
-
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-
-    it('should make a GET request', () => {
-        service.get('/api/test').subscribe(response => {
-            expect(response).toBeTruthy();
-        });
-        const req = httpMock.expectOne('/api/test');
-        expect(req.request.method).toBe('GET');
-        req.flush({});
-    });
-
-    it('should make a POST request', () => {
-        service.post('/api/test', { name: 'test' }).subscribe(response => {
-            expect(response).toBeTruthy();
-        });
-        const req = httpMock.expectOne('/api/test');
-        expect(req.request.method).toBe('POST');
-        req.flush({});
-    });
 });
