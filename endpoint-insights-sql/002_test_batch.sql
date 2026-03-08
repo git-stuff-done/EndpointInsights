78e5e3223b6fc1f936b0bd19865ddfc99b23f8a1
Cdog778 <cardellrankin@gmail.com>
2026-03-08 13:52:11 -0700

    EI-280

--- endpoint-insights-sql/002_test_batch.sql
+++ endpoint-insights-sql/002_test_batch.sql
@@ -1,13 +1,14 @@
 CREATE TABLE IF NOT EXISTS test_batch (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    batch_name TEXT NOT NULL,
-    schedule_id BIGINT,
-    start_time TIMESTAMP,
-    last_time_run TIMESTAMP,
     active BOOLEAN,
-    cron_expression TEXT,
-    created_by TEXT,
+    last_time_run TIMESTAMP(6),
+    start_time TIMESTAMP(6),
     created_date TIMESTAMPTZ,
-    updated_by TEXT NOT NULL,
-    updated_date TIMESTAMPTZ NOT NULL
+    schedule_id BIGINT,
+    updated_date TIMESTAMPTZ NOT NULL,
+    id UUID NOT NULL,
+    batch_name VARCHAR(255) NOT NULL,
+    created_by VARCHAR(255),
+    updated_by VARCHAR(255) NOT NULL,
+    cron_expression VARCHAR(255),
+    CONSTRAINT test_batch_pkey PRIMARY KEY (id)
 );
