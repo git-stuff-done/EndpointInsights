2f7cbedf12def02fbea3a1ca311b1fff9b87ecb4
Caleb Brock <cbrock@csus.edu>
2026-04-12 18:37:07 -0700

    EI-309 - Fix months value not being used

--- endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/service/TestRunSchedulerService.java
+++ endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/service/TestRunSchedulerService.java
@@ -44,6 +44,7 @@ public class TestRunSchedulerService {
 
     private void runPurge() {
         Instant oldestTestRun = Instant.now()
+                .minus(testRunsConfig.getMaxAgeMonths(), ChronoUnit.MONTHS)
                 .minus(testRunsConfig.getMaxAgeDays(), ChronoUnit.DAYS)
                 .minus(testRunsConfig.getMaxAgeHours(), ChronoUnit.HOURS)
                 .minus(testRunsConfig.getMaxAgeMinutes(), ChronoUnit.MINUTES)
