4975c7b6e28f56da138a5dd0e9c7ae615fc972e4
Tyler Mains <92330504+tmains515@users.noreply.github.com>
2026-04-12 22:08:32 -0700

    Revert "Feature/ei 315" (#169)
    Revert "Feature/ei 315 (#154)"
    
    This reverts commit e3389fdd6af825014e932684b28cf900534aba4c.

--- endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/service/JavaMailEmailSender.java
+++ endpoint-insights-api/src/main/java/com/vsp/endpointinsightsapi/service/JavaMailEmailSender.java
@@ -1,32 +1,12 @@
 package com.vsp.endpointinsightsapi.service;
 
-import com.vsp.endpointinsightsapi.model.Job;
-import com.vsp.endpointinsightsapi.model.entity.PerfTestResult;
-import com.vsp.endpointinsightsapi.model.entity.TestResult;
-import com.vsp.endpointinsightsapi.model.entity.TestRun;
-import com.vsp.endpointinsightsapi.model.enums.JobStatus;
-import com.vsp.endpointinsightsapi.model.enums.TestFailureTypes;
-import com.vsp.endpointinsightsapi.repository.JobRepository;
-import com.vsp.endpointinsightsapi.repository.PerfTestResultRepository;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.*;
+import java.util.UUID;
 
 @Service
 @Profile("!EMAIL-NOOP")
@@ -37,62 +17,18 @@ public class JavaMailEmailSender implements EmailSender {
     @Value("${spring.mail.from}")
     private String from;
 
-    private final ResourceLoader resourceLoader;
-    private final JobRepository jobRepository;
-
-    public JavaMailEmailSender(JavaMailSender mailSender, ResourceLoader resourceLoader, JobRepository jobRepository) {
+    public JavaMailEmailSender(JavaMailSender mailSender) {
         this.mailSender = mailSender;
-        this.resourceLoader = resourceLoader;
-        this.jobRepository = jobRepository;
     }
 
     @Override
-    public void sendTestCompletionEmail(String batchName, TestRun testRun, String recipientEmail, List<TestResult> results) throws IOException, MessagingException {
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-
-        Resource resource = resourceLoader.getResource("classpath:email_templates/failed_test_email_template.html");
-        Map<String, String> variablesMap = new HashMap<>();
-        PerfTestResult perf = new PerfTestResult();
-        String reasonForFailure = TestFailureTypes.OTHER.toString();
-        for(TestResult testResult : results){
-            List<PerfTestResult> perfTestResults = testResult.getPerfTestResult();
-            for(PerfTestResult perfTestResult : perfTestResults){
-                if(perfTestResult.getTestResult().getId().equals(testResult.getId()) && perfTestResult.getJobId().equals(testRun.getJobId()) && perfTestResult.getLatencyThresholdResult().equals(JobStatus.FAIL.name()) ){
-                    perf = perfTestResult;
-                    reasonForFailure = TestFailureTypes.LATENCY_THRESHOLD_EXCEEDED.toString();
-                }
-            }
-        }
-        Optional<Job> job =  jobRepository.findById(testRun.getJobId());
-        if (job.isEmpty()) {
-            throw new EntityNotFoundException("Job with id " + testRun.getJobId() + " not found");
-        }
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy h:mm a")
-                .withZone(ZoneId.of("America/Los_Angeles"));
-
-        variablesMap.put("name", batchName);
-        variablesMap.put("date", formatter.format(LocalDateTime.now()));
-        variablesMap.put("startTime", formatter.format(testRun.getStartedAt()));
-        variablesMap.put("endTime", formatter.format(testRun.getFinishedAt()));
-        variablesMap.put("reason", reasonForFailure);
-        variablesMap.put("jobName", job.get().getName());
-        variablesMap.put("P50", perf.getP50LatencyMs() + "ms");
-        variablesMap.put("P95", perf.getP95LatencyMs() + "ms");
-        variablesMap.put("P99", perf.getP99LatencyMs() + "ms");
-        variablesMap.put("threshold", job.get().getThreshold().toString() + "ms");
-
-        String template = new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
-
-        for (String variable : variablesMap.keySet()) {
-            template = template.replace("{" + variable + "}", variablesMap.get(variable));
-        }
-
-        helper.setFrom(from);
-        helper.setTo(recipientEmail);
-        helper.setSubject("Test Run Completed - " + testRun.getRunId());
-        helper.setText(template, true);
-        mailSender.send(message);
+    public void sendTestCompletionEmail(UUID runId, UUID resultId, String recipientEmail) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(recipientEmail);
+        message.setSubject("Test Run Completed - " + runId);
+        message.setText(buildEmailBody(runId, resultId));
+       mailSender.send(message);
     }
 
     private String buildEmailBody(UUID runId, UUID resultId) {
