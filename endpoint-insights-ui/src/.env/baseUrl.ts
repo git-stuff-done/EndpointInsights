bb8e742dd40fd4dd816a7e4a51f268f0644d8be2
Morningstar515 <t.mains515@hotmail.com>
2026-04-25 15:34:33 -0700

    corrections tests

--- endpoint-insights-ui/src/.env/baseUrl.ts
+++ endpoint-insights-ui/src/.env/baseUrl.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:8080/oauth2/authorization/oidc'
+    apiUrl: 'http://localhost:8080/oauth2/authorization/oidc'
 };
